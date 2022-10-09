4d1b3e45cfa5299cf29c396788111635472c0fa3
WiTe0DeViL <surentamarai222@gmail.com>
2022-10-09 11:07:40 +0530

    Updates

--- Intelij/src/WeeklyAssessment/FrequencyChecker.java
+++ Intelij/src/WeeklyAssessment/FrequencyChecker.java
@@ -1,3 +1,5 @@
+package WeeklyAssessment;
+
 import java.util.HashMap;
 import java.util.Map;
 
--- Intelij/src/WeeklyAssessment/MaximumSubsequence.java
+++ Intelij/src/WeeklyAssessment/MaximumSubsequence.java
@@ -1,3 +1,5 @@
+package WeeklyAssessment;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
